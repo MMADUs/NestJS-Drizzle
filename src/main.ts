b78ced1a76e1dd070ffd34fe3935fe2989ef4406
MMADUs <m.nizwa01@gmail.com>
2024-10-07 19:06:32 +0700

    runtime is now multithreaded.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
+
+const cluster = require('cluster');
+const os = require('os');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.use(cookieParser());
-  await app.listen(3000);
+  const numCPUs = os.cpus().length;
+
+  if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+
+    // Fork workers
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} died`);
+      // Replace the dead worker
+      cluster.fork();
+    });
+  } else {
+    // Workers can share any TCP connection
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(new ValidationPipe());
+    app.use(cookieParser());
+
+    await app.listen(3000);
+    console.log(`Worker ${process.pid} started`);
+  }
 }
+
 bootstrap();
